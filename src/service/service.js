eecd797ce695c63a1da243f56f9d59fa9211b37e
Rosivaldo de Souza Silva <rosivaldoedeus@gmail.com>
2020-07-07 13:18:31 -0300

    test0

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 //url base
-const api = axios.create({baseURL: process.env.REACT_APP_API_URL });
+const api = axios.create({baseURL: 'http://localhost:5000/sistema' });
 
 export default api;
